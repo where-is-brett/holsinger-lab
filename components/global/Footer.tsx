3cb5b087c1336f0feee3a0ec60e6fb12858d190b
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-29 14:45:17 +1000

    Clean up

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,7 +4,7 @@ import { CustomPortableText } from 'components/shared/CustomPortableText'
 
 export function Footer({ footer }: { footer?: PortableTextBlock[] }) {
   return (
-    <footer className="border-t font-antarctican bottom-0 w-full bg-background py-5 text-center lg:py-6">
+    <footer className="border-t border-primary font-antarctican bottom-0 w-full bg-background py-5 text-center lg:py-6">
       {footer && (
         <CustomPortableText
           paragraphClasses="text-md md:text-xl"
