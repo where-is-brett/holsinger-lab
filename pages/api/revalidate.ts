a1cc9892d1eaca5fa449a01e520d54b3bfdd9dce
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-30 16:36:24 +1000

    Update revalidate.ts

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,17 +5,24 @@ export { config } from 'next-sanity/webhook'
 export default async function handler(req, res) {
 
   try {
-    const {isValidSignature, body} = await parseBody(req, process.env.SANITY_WEBHOOK_SECRET)
+    const { isValidSignature, body } = await parseBody(req, process.env.SANITY_WEBHOOK_SECRET)
     const { type, slug } = body
 
     if (!isValidSignature) {
       const message = 'Invalid signature'
       console.warn(message)
-      res.status(401).json({message})
+      res.status(401).json({ message })
       return
     }
 
     switch (type) {
+      case 'home':
+        await res.revalidate(`/`)
+        console.log(`Revalidated "${type}"`)
+        return res.status(200).json({
+          success: true,
+          message: `Revalidated "${type}"`,
+        })
       case 'page':
         await res.revalidate(`/${slug}`)
         console.log(`Revalidated "${type}" with slug "${slug}"`)
@@ -31,14 +38,14 @@ export default async function handler(req, res) {
           message: `Revalidated "${type}" with slug "${slug}"`,
         })
       case 'publications':
-        await res.revalidate(`/publications`)
+        await res.revalidate(`/publications/`)
         console.log(`Revalidated "${type}"`)
         return res.status(200).json({
           success: true,
           message: `Revalidated "${type}" with slug "publications"`,
         })
       case 'profile':
-        await res.revalidate(`/people`)
+        await res.revalidate(`/people/`)
         console.log(`Revalidated "${type}"`)
         return res.status(200).json({
           success: true,
