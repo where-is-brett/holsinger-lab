2ec0668287bf0aefc42fa9accafd2c7164186a19
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-30 16:12:17 +1000

    Upgrade revalidate api

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,130 +1,55 @@
-// import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
-// import type { NextApiRequest, NextApiResponse } from "next"
+import { parseBody } from 'next-sanity/webhook'
+export { config } from 'next-sanity/webhook'
 
-// type Data = {
-//   success: boolean
-//   message: string
-// }
-
-// const secret = process.env.SANITY_WEBHOOK_SECRET
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-//   if (req.method !== "POST") {
-//     console.error("Must be a POST request")
-//     return res.status(401).json({ success: false, message: "Must be a POST request" })
-//   }
-
-//   const signature = req.headers[SIGNATURE_HEADER_NAME];
-//   const isValid = isValidSignature(JSON.stringify(req.body), signature, secret);
-
-//   if (!isValid) {
-//     res.status(401).json({ success: false, message: "Invalid signature" })
-//     return
-//   }
-
-//   try {
-//     const {
-//       body: { type, slug },
-//     } = req
-
-//     switch (type) {
-//       case "page":
-//         await res.revalidate(`/${slug}`)
-//         console.log(`Revalidated "${type}" with slug "${slug}"`)
-//         return res.json({ success: true, message: `Revalidated "${type}" with slug "${slug}"` })
-//       case "project":
-//         await res.revalidate(`/projects/${slug}`)
-//         console.log(`Revalidated "${type}" with slug "${slug}"`)
-//         return res.json({ success: true, message: `Revalidated "${type}" with slug "${slug}"` })
-//       case "publications":
-//         await res.revalidate(`/publications`)
-//         console.log(`Revalidated "${type}"`)
-//         return res.json({ success: true, message: `Revalidated "${type}" with slug "publications"` })
-//       case "profile":
-//         await res.revalidate(`/people`)
-//         console.log(`Revalidated "${type}"`)
-//         return res.json({ success: true, message: `Revalidated "${type}" with slug "people"` })
-//     }
-
-//     return res.json({ success: false, message: "No managed type" })
-//   } catch (err) {
-//     return res.status(500).send({ success: false, message: "Error revalidating" })
-//   }
-// }
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// }
-
-import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
-
-const secret = process.env.SANITY_WEBHOOK_SECRET
 
 export default async function handler(req, res) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME]
-  const body = await readBody(req) // Read the body into a string
-  if (!isValidSignature(body, signature, secret)) {
-    res.status(401).json({ success: false, message: 'Invalid signature' })
-    return
-  }
 
-  const jsonBody = JSON.parse(body)
-  console.log(jsonBody)
   try {
-    const { type, slug } = jsonBody
+    const {isValidSignature, body} = await parseBody(req, process.env.SANITY_WEBHOOK_SECRET)
+    const { type, slug } = body
+
+    if (!isValidSignature) {
+      const message = 'Invalid signature'
+      console.warn(message)
+      res.status(401).json({message})
+      return
+    }
 
     switch (type) {
       case 'page':
         await res.revalidate(`/${slug}`)
         console.log(`Revalidated "${type}" with slug "${slug}"`)
-        return res.json({
+        return res.status(200).json({
           success: true,
           message: `Revalidated "${type}" with slug "${slug}"`,
         })
       case 'project':
         await res.revalidate(`/projects/${slug}`)
         console.log(`Revalidated "${type}" with slug "${slug}"`)
-        return res.json({
+        return res.status(200).json({
           success: true,
           message: `Revalidated "${type}" with slug "${slug}"`,
         })
       case 'publications':
         await res.revalidate(`/publications`)
         console.log(`Revalidated "${type}"`)
-        return res.json({
+        return res.status(200).json({
           success: true,
           message: `Revalidated "${type}" with slug "publications"`,
         })
       case 'profile':
         await res.revalidate(`/people`)
         console.log(`Revalidated "${type}"`)
-        return res.json({
+        return res.status(200).json({
           success: true,
           message: `Revalidated "${type}" with slug "people"`,
         })
     }
 
     return res.json({ success: false, message: 'No managed type' })
+
   } catch (err) {
-    return res
-      .status(500)
-      .send({ success: false, message: 'Error revalidating' })
+    console.error(err)
+    return res.status(500).json({ success: false, message: 'Error revalidating' })
   }
-}
-
-// Next.js will by default parse the body, which can lead to invalid signatures
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
-
-async function readBody(readable) {
-  const chunks = []
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
-  }
-  return Buffer.concat(chunks).toString('utf8')
 }
