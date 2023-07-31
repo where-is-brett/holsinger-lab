8ab08733f09cee632f346356e4cd2ec331912172
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-31 17:14:52 +1000

    Add global revalidation as fall back

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,8 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { parseBody } from 'next-sanity/webhook'
 export { config } from 'next-sanity/webhook'
 
+import {
+  pagePaths,
+  projectPaths
+} from 'lib/sanity.queries'
+import { getClient } from 'lib/sanity.client'
+import { resolveHref } from 'lib/sanity.links'
 
-export default async function handler(req, res) {
+export const getAllPaths = async (staticPaths: string[] = ['/', '/publications', '/contact', '/people']) => {
+  const client = getClient()
+  const pages = await client.fetch<string[]>(pagePaths)
+  const projects = await client.fetch<string[]>(projectPaths)
+  const paths = [...pages.map(slug => resolveHref('page', slug)), ...projects.map(slug => resolveHref('project', slug))]
+  return [...staticPaths, ...paths]
+
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   try {
     const { isValidSignature, body } = await parseBody(req, process.env.SANITY_WEBHOOK_SECRET)
@@ -47,8 +63,19 @@ export default async function handler(req, res) {
           success: true,
           message: `Revalidated "${type}" with slug "people"`,
         })
+      default:
+        const paths = await getAllPaths();
+        await Promise.all(paths.map(async (path) => {
+          path && await res.revalidate(path);
+          console.log(`Revalidating ${path}...`)
+        }));
+        return res.status(200).json({
+          success: true,
+          message: `Revalidated all pages.`,
+        })
+
     }
-    
+
     return res.json({ success: false, message: 'No managed type' })
 
   } catch (err) {
